411798e5bb7356fa176d126acff7c10e141cf95e
Elecktry <100728162+Elecktry@users.noreply.github.com>
2022-03-16 22:00:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -15,4 +15,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-client.run('OTUyMzM0OTc1NTI3Njk4NTIz.Yi0hDw.bbmihylhnUYeQRDKzc6LnAb5Mdw')
+client.run('Seu token')
